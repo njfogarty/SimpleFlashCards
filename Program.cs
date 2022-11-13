82deef0dcf4d68a5d94fd5ef8f1240f4eaf0e1d4
Noah Fogarty <nojosoft@gmail.com>
2022-11-12 22:52:45 -0500

    Multiplication Deck
    Heavily improved build for multiplication. Allows user defined multiplication base (Example: '? X 1-12', where '?' is user defined). Allows repetition without running the program again.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ Random rnd = new Random();
 #endregion
 
 #region variables
+int multiplyBy;
+
 int cardSelect; // Holds a randomly generated number that is used to select which card to display next.
 int cardAnswer; // Users answer.
 
@@ -38,12 +40,25 @@ bool debug = false; // !!!DEBUG MODE!!!
 #endregion
 
 #region console_output
-while (cardsFinished == false) // If user has gone through all cards, this will stop looping.
+Console.Write("What would you like to multiply by?: ");
+multiplyBy = Convert.ToInt32(Console.ReadLine());
+Console.Write("\n");
+while (!cardsFinished) // If user has gone through all cards, this will stop looping.
 {
     // Determines whether all cards have been completed or not.
     if (card01 && card02 && card03 && card04 && card05 && card06 && card07 && card08 && card09 && card10 && card11 && card12)
     {
-        cardsFinished = true;
+        Console.Write("\nDeck finished! Would you like to continue?: ");
+        string restartChoice = Console.ReadLine();
+        if (restartChoice == "Yes" || restartChoice == "yes")
+        {
+            card01 = card02 = card03 = card04 = card05 = card06 = card07 = card08 = card09 = card10 = card11 = card12 = false;
+            
+            Console.Write("What would you like to multiply by?: ");
+            multiplyBy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+        }
+        else cardsFinished = true;
     }
 
     cardAnswer = 0; // Resets answer.
@@ -58,130 +73,129 @@ while (cardsFinished == false) // If user has gone through all cards, this will 
     // This is also where you would change the cards contents, as well as add or remove cards.
 
     #region cards
-    if (cardSelect == 1 && !card01) // 3 X 1
+    if (cardSelect == 1 && !card01) // multiplyBy X 1
     {
-        while (cardAnswer != 3)
+        while (cardAnswer != multiplyBy * 1)
         {
-            Console.Write("What is 3 X 1?: ");
+            Console.Write($"What is {multiplyBy} X 1?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card01 = true;
     }
 
-    else if (cardSelect == 2 && !card02) // 3 X 2
+    else if (cardSelect == 2 && !card02) // multiplyBy X 2
     {
-        while (cardAnswer != 6)
+        while (cardAnswer != multiplyBy * 2)
         {
-            Console.Write("What is 3 X 2?: ");
+            Console.Write($"What is {multiplyBy} X 2?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card02 = true;
     }
 
-    else if (cardSelect == 3 && !card03) // 3 x 3
+    else if (cardSelect == 3 && !card03) // multiplyBy x 3
     {
-        while (cardAnswer != 9)
+        while (cardAnswer != multiplyBy * 3)
         {
-            Console.Write("What is 3 X 3?: ");
+            Console.Write($"What is {multiplyBy} X 3?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card03 = true;
     }
 
-    else if (cardSelect == 4 && !card04) // 3 X 4
+    else if (cardSelect == 4 && !card04) // multiplyBy X 4
     {
-        while (cardAnswer != 12)
+        while (cardAnswer != multiplyBy * 4)
         {
-            Console.Write("What is 3 X 4?: ");
+            Console.Write($"What is {multiplyBy} X 4?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card04 = true;
     }
     
-    else if (cardSelect == 5 && !card05) // 3 X 5
+    else if (cardSelect == 5 && !card05) // multiplyBy 3 X 5
     {
-        while (cardAnswer != 15)
+        while (cardAnswer != multiplyBy * 5)
         {
-            Console.Write("What is 3 X 5?: ");
+            Console.Write($"What is {multiplyBy} X 5?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card05 = true;
     }
 
-    else if (cardSelect == 6 && !card06) // 3 X 6
+    else if (cardSelect == 6 && !card06) // multiplyBy X 6
     {
-        while (cardAnswer != 18)
+        while (cardAnswer != multiplyBy * 6)
         {
-            Console.Write("What is 3 X 6?: ");
+            Console.Write($"What is {multiplyBy} X 6?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card06 = true;
     }
 
-    else if (cardSelect == 7 && !card07) // 3 X 7
+    else if (cardSelect == 7 && !card07) // multiplyBy X 7
     {
-        while (cardAnswer != 21)
+        while (cardAnswer != multiplyBy * 7)
         {
-            Console.Write("What is 3 X 7?: ");
+            Console.Write($"What is {multiplyBy} X 7?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card07 = true;
     }
 
-    else if (cardSelect == 8 && !card08) // 3 X 8
+    else if (cardSelect == 8 && !card08) // multiplyBy X 8
     {
-        while (cardAnswer != 24)
+        while (cardAnswer != multiplyBy * 8)
         {
-            Console.Write("What is 3 X 8?: ");
+            Console.Write($"What is {multiplyBy} X 8?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card08 = true;
     }
 
-    else if (cardSelect == 9 && !card09) // 3 X 9
+    else if (cardSelect == 9 && !card09) // multiplyBy X 9
     {
-        while (cardAnswer != 27)
+        while (cardAnswer != multiplyBy * 9)
         {
-            Console.Write("What is 3 X 9?: ");
+            Console.Write($"What is {multiplyBy} X 9?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card09 = true;
     }
 
-    else if (cardSelect == 10 && !card10) // 3 X 10
+    else if (cardSelect == 10 && !card10) // multiplyBy X 10
     {
-        while (cardAnswer != 30)
+        while (cardAnswer != multiplyBy * 10)
         {
-            Console.Write("What is 3 X 10?: ");
+            Console.Write($"What is {multiplyBy} X 10?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card10 = true;
     }
 
-    else if (cardSelect == 11 && !card11) // 3 X 11
+    else if (cardSelect == 11 && !card11) // multiplyBy 3 X 11
     {
-        while (cardAnswer != 33)
+        while (cardAnswer != multiplyBy * 11)
         {
-            Console.Write("What is 3 X 11?: ");
+            Console.Write($"What is {multiplyBy} X 11?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card11 = true;
     }
 
-    else if (cardSelect == 12 && !card12) // 3 X 12
+    else if (cardSelect == 12 && !card12) // multiplyBy X 12
     {
-        while (cardAnswer != 36)
+        while (cardAnswer != multiplyBy * 12)
         {
-            Console.Write("What is 3 X 12?: ");
+            Console.Write($"What is {multiplyBy} X 12?: ");
             cardAnswer = Convert.ToInt32(Console.ReadLine());
         }
         card12 = true;
     }
     #endregion
 }
-// Alerts user that deck has been finished.
-Console.Beep();
-Console.WriteLine("\nDeck completed!");
 
+Console.Clear();
+Console.WriteLine("Press any key to exit...");
 Console.ReadKey(true); // Pauses program.
 #endregion
